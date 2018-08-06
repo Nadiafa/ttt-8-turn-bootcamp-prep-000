c2d32f635548afe6b588734e02fe675324311e83
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-06 14:53:10 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,2 +1,18 @@
 #helper methods
+ 
 def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(position)
+  indexed_position = position.to_i
+  return indexed_position - 1
+end
+
+def move(board, index, player_character="X")
+  board[index] = player_character
+end
