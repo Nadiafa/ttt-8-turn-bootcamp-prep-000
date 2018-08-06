124477a724868130d7042f73810c62283831d540
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-06 14:54:01 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,3 +16,16 @@ end
 def move(board, index, player_character="X")
   board[index] = player_character
 end
+
+def valid_move?(board, index)
+  if !position_taken?(board, index) && index.between?(0,8)
+    true
+  else
+    false
+  end    
+end
+
+def position_taken?(board, index)
+  board[index] =='' || board[index] ==' '|| board[index] == nil
+  board[index] =='X' || board[index] =='O'
+end
